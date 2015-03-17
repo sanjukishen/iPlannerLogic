7323bd3000f3e8743ba85ee0104ed65b4e80d8b8
Shri Kishen Rajendran <sanjukishen@gmail.com>
2015-03-17 17:08:34 +0800

    Dummy change

--- iPlanner/iPlanner.cpp
+++ iPlanner/iPlanner.cpp
@@ -4,63 +4,6 @@
 #include "stdafx.h"
 #include "iPlanner.h"
 
-using namespace std;
-
-void iPlanner::clearTasks(void) {
-  for (int i = 0; i < freePos; ++i) {
-    taskList[i].erase();
-    taskDate[i].erase();
-    taskTime[i].erase();
-  }
-   
-}
-
-
-// manageiPlanner takes user input and manipulates the 
-// iPlanner class object.
-int manageiPlanner(char *fileName) {
-  // Variable to denote successful processing of function
-  int retCode = -1;
-  if (fileName) {
-    iPlanner ip;
-    ip.loadFile(fileName); 
-
-    // Variables to obtain input from user and separate
-    // into command word and remaining string
-    string ipStr;
-    string commandWord;
-	  string remString;
-
-    while (1) { 
-      cout << endl << "command: ";
-      getline(cin, ipStr);
-
-      // Parse the user input string to separate
-      // action and rest of the string.
-      int pos = ipStr.find_first_of(" \t");  
-      commandWord.assign(ipStr.substr(0, pos));   
-      
-      if (pos > 0) {
-        pos = ipStr.find_first_not_of(" \t", pos);
-
-        // Required for 'add' and 'delete' command.
-        remString.assign(ipStr.substr(pos, -1));    
-      }
-
-      // Transform all characters to lower case for case insensitive comparision.
-      std::transform(commandWord.begin(), commandWord.end(), commandWord.begin(), ::tolower);
-      if (commandWord.compare("exit") == 0) {
-        retCode = ip.saveFile(fileName);
-        break;
-      }
-      else {
-        ip.processCommand(commandWord, remString);
-    }
-  }
-  return retCode;
-  }
-}
-
 int _tmain(int argc, _TCHAR* argv[])
 {
 	return 0;
--- iPlanner/iPlanner.h
+++ iPlanner/iPlanner.h
@@ -9,11 +9,13 @@
 
 #define SIZE 1024
 
+using namespace std;
+
 class iPlanner {
 
-  std::array<std::string, SIZE> taskList;
-  std::array<std::string, SIZE> taskDate;
-  std::array<std::string, SIZE> taskTime;
+  array<string, SIZE> taskList;
+  array<string, SIZE> taskDate;
+  array<string, SIZE> taskTime;
 
   int freePos;
 
@@ -23,16 +25,16 @@ public:
   void printMessage(std::string message);
   void processCommand(std::string tempStr1, std::string tempStr2);
 
-  int addTask(std::string& strData);
+  int addTask(string& strData);
   int removeTask(int pos, std::string& strData);
-  int editTask(std::string& strData);
-  int rescheduleTask(std::string& strData);
+  int editTask(string& strData);
+  int rescheduleTask(string& strData);
   int undoLastAction(void);
-  int specifyDeadLine(std::string& strData);
+  int specifyDeadLine(string& strData);
   void clearTasks(void);
-  int addToArchive(std::string& strData);
-  int markAsComplete(std::string& strData);
-  int chooseSaveFolder(std::string& strData);
+  int addToArchive(string& strData);
+  int markAsComplete(string& strData);
+  int chooseSaveFolder(string& strData);
   int viewTasks(void);
   int searchTask(std::string& word);
   int labelItem(std::string& strData);
